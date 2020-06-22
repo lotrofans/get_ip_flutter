3c02f5631fc7875b712c056a50ac4aec92c909d8
edmund <edmundtay96@gmail.com>
2020-06-22 14:47:31 +0800

    fix: added missing end statement

--- ios/Classes/GetIpPlugin.m
+++ ios/Classes/GetIpPlugin.m
@@ -6,6 +6,7 @@
 // is not copied when this plugin is created as a library.
 // https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
 #import "get_ip-Swift.h"
+#endif
 
 @implementation GetIpPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
